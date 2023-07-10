e077d6cd836b50a077a541ab06eab8c9acac0b58
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-07-10 12:38:32 +0300

    final-dep

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
-console.log(galleryItems);
-
-const instance = basicLightbox.create(`
-    <h1>Dynamic Content</h1>
-    <p>You can set the content of the lightbox with JS.</p>
-`);
-//instance.show()
-
-//============js==============
 
 const galleryBox = document.querySelector(".gallery");
-console.log(galleryBox);
 
 const markup = galleryItems
   .map(
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,40 +1,17 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
-console.log(galleryItems);
 
 const galleryBox = document.querySelector(".gallery");
-console.log(galleryBox);
 
-const markup = galleryItems
-  .map(
-    ({ preview, original, description }) => `<li class="gallery__item">
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
        <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
  </li>`
-  )
-  .join("");
+);
+galleryBox.insertAdjacentHTML("beforeend", markup.join(""));
 
-galleryBox.insertAdjacentHTML("beforeend", markup);
-
-galleryBox.addEventListener("click", onImageClick);
-
-function onImageClick(evt) {
-  evt.preventDefault();
-
-  let description = evt.srcElement.alt;
-
-  if (!evt.target.classList.contains("gallery__image")) {
-    return;
-  }
- 
-
-    new SimpleLightbox(".gallery a", {
-   
-    captionType: description,
-  
-    captionDelay: 250,
-  });
-  
-}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
